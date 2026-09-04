7cfc32279d6084a04770f4ba514689bd119531a4
ZachCapillo <zpcapillo2030@clc.pshs.edu.ph>
2026-09-04 21:08:23 +0800

    Add files via upload

---
+++ Q1/q1_sg6_Balingkilat_Capillo.py
@@ -0,0 +1,15 @@
+class Lab:
+    def __init__(self, room_number):
+        self.room_number = room_number
+
+class Technician:
+    def __init__(self, name):
+        self.name = name
+        self.assigned_lab = None
+    def assigned_lab(self, lab_obj):
+        self.assigned_lab = lab_obj
+        
+chem_lab = Lab("302")    
+mr_cruz = Technician("Mr. Cruz")
+mr_cruz.assigned_lab = chem_lab
+print(mr_cruz.assigned_lab.room_number)
